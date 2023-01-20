e81559ccc58ec4e2f20d86cea014b4165568c9ce
Gabriel <gabrielmurryedh@gmail.com>
2023-01-20 14:53:55 -0800

    initialized repo for sudoku solver frontend

---
+++ app.js
@@ -0,0 +1,80 @@
+const puzzleBoard = document.querySelector("#puzzle");
+const solveButton = document.querySelector("#solve-button");
+const solutionDisplay = document.querySelector("#solution");
+const squares = 81;
+let submission = [];
+
+for (let i = 0; i < squares; i++) {
+  const inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "number");
+  inputElement.setAttribute("min", "1");
+  inputElement.setAttribute("max", "9");
+  if (
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+    ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+  ) {
+    inputElement.classList.add("odd-section");
+  }
+
+  puzzleBoard.appendChild(inputElement);
+}
+
+const joinValues = () => {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    if (input.value) {
+      submission.push(input.value);
+    } else {
+      submission.push(".");
+    }
+  });
+  console.log(submission);
+};
+
+const populateValues = (isSolvable, solution) => {
+  const inputs = document.querySelectorAll("input");
+  if (isSolvable && solution) {
+    inputs.forEach((input, i) => {
+      input.value = solution[i];
+    });
+    solutionDisplay.innerHTML = "Solved!";
+  } else {
+    solutionDisplay.innerHTML = "Not Solvable";
+  }
+};
+
+const solve = () => {
+  joinValues();
+  // data needs to be an object
+  const data = { numbers: submission.join("") };
+  console.log("data", data);
+
+  // calling our own backend. We want to hide our API key
+  fetch(
+    "https://coruscating-empanada-3055a5.netlify.app/.netlify/functions/api/solve",
+    {
+      // http://localhost:8000/solve for dev
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      // body is going to the server
+      body: JSON.stringify(data),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      populateValues(data.solvable, data.solution);
+      submission = [];
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+solveButton.addEventListener("click", solve);
